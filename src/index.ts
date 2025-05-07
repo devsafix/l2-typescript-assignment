3dc0d9911bded6e820f0f43e14b27ab89988ebe1
devsafix <devsafix@gmail.com>
2025-05-08 02:01:41 +0600

    Problem 8 solved

--- src/index.ts
+++ src/index.ts
@@ -118,3 +118,20 @@ function getDayType(day: Day): string {
 
 getDayType(Day.Monday);
 getDayType(Day.Sunday);
+
+async function squareAsync(n: number): Promise<number> {
+  const promise: Promise<number> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (n > 0) {
+        resolve(n * n);
+      } else {
+        reject("Error: Negative number not allowed");
+      }
+    }, 1000);
+  });
+
+  return promise;
+}
+
+squareAsync(4).then(console.log);
+squareAsync(-3).catch(console.error);
